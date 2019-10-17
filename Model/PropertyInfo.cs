adccfaecb1b2ff6ae63756917de85a241e29e13f
Kaylyn Eheman <kaylyneheman@gmail.com>
2019-10-16 22:23:20 -0400

    added PropertyInfo class to Model class library

---
+++ Model/PropertyInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class PropertyInfo
+    {
+        public double size { get; set; }
+        public int numBeds { get; set; }
+        public int numBaths { get; set; }
+        public string desc { get; set; }
+        public List<string> photos { get; set; }
+        public List<double> priceLight { get; set; }
+        public List<double> priceNormal { get; set; }
+        public List<double> priceHeavy { get; set; }
+
+        public PropertyInfo()
+        {
+            priceLight = new List<double>();
+            priceNormal = new List<double>();
+            priceHeavy = new List<double>();
+        }
+
+        public static double getAverage(List<double> priceList)
+        {
+            if (priceList.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                double avg = 0;
+                foreach (int p in priceList)
+                {
+                    avg += p;
+                }
+                return avg / priceList.Count;
+            }
+        }
+
+    }
+}
--- UMaidConsole/Program.cs
+++ UMaidConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace UMaidConsole
 {
@@ -10,6 +11,23 @@ namespace UMaidConsole
     {
         static void Main(string[] args)
         {
+            PropertyInfo house = new PropertyInfo();
+            house.numBeds = 4;
+            house.numBaths = 2;
+            house.size = 1200;
+
+            Console.WriteLine("This house has " + house.numBeds + " bedrooms, " + house.numBaths +
+                " bathrooms, and is " + house.size + " sq. feet.");
+
+            Console.WriteLine("The average cost is " + PropertyInfo.getAverage(house.priceHeavy) + ".");
+
+            house.priceHeavy.Add(300.00);
+            house.priceHeavy.Add(200.00);
+            house.priceHeavy.Add(300.00);
+            house.priceHeavy.Add(200.00);
+            house.priceHeavy.Add(1000.00);
+
+            Console.WriteLine("The average cost is " + PropertyInfo.getAverage(house.priceHeavy) + ".");
         }
     }
 }
